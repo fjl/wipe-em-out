245271067856a5e0aefe4e5498b6c78d3b1d2102
Ephraim <es.90@gmx.de>
2025-09-20 15:15:52 +0200

    added new version - created a wipe out scene for testing

---
+++ Assets/Scripts/MoveMouse.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class MoveMouse : MonoBehaviour
+{
+    public Camera mainCamera;
+    [Header("Input")]
+    public InputActionProperty LookPosAction;
+    public Transform cursor;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPoint = Vector3.zero;
+        Ray ray = mainCamera.ScreenPointToRay(LookPosAction.action.ReadValue<Vector2>());
+        Plane ground = new Plane(Vector3.forward, Vector3.zero);
+        if (ground.Raycast(ray, out float dist))
+        {
+            targetPoint = ray.GetPoint(dist);
+            Vector3 dir = targetPoint - transform.position;
+            dir.y = 0;
+            //if (dir.sqrMagnitude > 0.01f)
+            //    transform.rotation = Quaternion.LookRotation(dir.normalized);
+        }
+
+        cursor.position = targetPoint;
+    }
+}
